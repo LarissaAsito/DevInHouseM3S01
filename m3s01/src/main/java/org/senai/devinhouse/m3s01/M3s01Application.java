a2e7f8701e4d461575b1444954bae7a930e9191f
Larissa <larissa.asito@gmail.com>
2023-05-07 01:42:42 -0300

    Adicionando exercicio 2

---
+++ m3s01/src/main/java/org/senai/devinhouse/m3s01/M3s01Application.java
@@ -0,0 +1,13 @@
+package org.senai.devinhouse.m3s01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M3s01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M3s01Application.class, args);
+	}
+
+}
---
+++ m3s01/src/main/java/org/senai/devinhouse/m3s01/models/Veiculo.java
@@ -0,0 +1,25 @@
+package org.senai.devinhouse.m3s01.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Veiculo {
+
+    @Id
+    private String placa;
+
+    private String tipo;
+
+    private String cor;
+
+    private Integer anoDeFabricacao;
+
+    private Integer qtdMultas;
+}
